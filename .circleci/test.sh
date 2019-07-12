ad4c8ff179c4c51a8c4de20475fd29dba6804727
karita <shigekikarita@gmail.com>
2019-07-12 12:51:53 +0900

    add test

---
+++ .circleci/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+# git submodule sync
+# git submodule update --init
+
+source "$(curl -fsS  --retry 3 https://dlang.org/install.sh | bash -s $1 --activate)"
+dub test --build=unittest-cov
+cat dub.selections.json
+
+# ldc2 causes linker error with drepl https://github.com/dlang-community/drepl/issues/39
+if [ "$DC" = dmd ]; then
+    bash <(curl -s https://codecov.io/bash) -s "source-grain-*.lst";
+    make doc;
+fi
+
